2d910ace450a4ed4e443e80174462e841f669a5c
*Alok Kumar <Alok87573770@gmail.com>
2025-05-18 05:33:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1747540683060.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_57cbe639._.js
        - https://6000-firebase-studio-1747540683060.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1747540683060.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Update the following images in this component:
    
    Image 1:
             - Original image: /_next/image?url=%2Fimages%2Fgames%2Fbubble-popper-preview.png&w=1200&q=75
             - New image URL: https://images.unsplash.com/photo-1504177847824-d075f5706770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxOHx8cG9wJTIwYnViYmxlfGVufDB8fHx8MTc0NzU0NjM0Mnww&ixlib=rb-4.1.0&q=80&w=1080
             - Hint used: "bubbles water"
    
    Replace the image sources with the new URLs. This may involve updating src, srcset, background-image, or other relevant attributes depending on the element type.

--- src/lib/imagePaths.ts
+++ src/lib/imagePaths.ts
@@ -4,10 +4,11 @@
  * Images should be placed in the `public` directory at the root of your project.
  * For example, an image with path '/images/games/bubble-popper-preview.png'
  * should be located at `public/images/games/bubble-popper-preview.png`.
+ * External URLs can also be used here, provided the hostname is whitelisted in next.config.js.
  */
 
 export const GAME_PREVIEW_IMAGES: Record<string, string> = {
-  'bubble-popper': '/images/games/bubble-popper-preview.png',
+  'bubble-popper': 'https://images.unsplash.com/photo-1504177847824-d075f5706770?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxOHx8cG9wJTIwYnViYmxlfGVufDB8fHx8MTc0NzU0NjM0Mnww&ixlib=rb-4.1.0&q=80&w=1080',
   'melt-away': '/images/games/melt-away-preview.png',
   // Add more game IDs and their image paths here as you add games.
   // Example:
